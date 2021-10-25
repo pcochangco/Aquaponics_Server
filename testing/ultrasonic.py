d7c3e338edd5ef3c2a0c2dcddb33ea1de0db7496
Philip Justin Cochangco <88139560+pcochangco@users.noreply.github.com>
2021-10-25 09:26:47 +0800

    Add files via upload

---
+++ testing/ultrasonic.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sep 25 09:05:03 2021
+#reference https://thepihut.com/blogs/raspberry-pi-tutorials/hc-sr04-ultrasonic-range-sensor-on-the-raspberry-pi
+@author: pcochang
+"""
+
+def GPIOinit():
+    import RPI.GPIO as GPIO import time 
+    GPIO.setmode(GPIO.BCM)
+    TRIG = 23 ECHO = 24
+    GPIO.setup (TRIG, GPIO.OUT, initial=GPIO.LOW) 
+    GPIO.setup (ECHO, GPIO.IN)
+    
+    
+def measureDistance_cm(TRIG = 23 ,ECHO = 24):
+    GPIO.output (TRIG, True) 
+    time.sleep(0.00001) 
+    GPIO.output (TRIG, False)
+    
+    while GPIO.input(ECHO)==0:
+        pulse_start = time.time()
+    while GPIO.input(ECHO)==1:
+        pulse_end = time.time()
+    
+    pulse_duration = pulse_end - pulse_start
+    distance = pulse_duration * 17150
+    distance = round (distance, 2)    
+    return distance
