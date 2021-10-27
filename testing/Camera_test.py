9e241ccb05997f5487534e891dc7fa4898d8142b
Philip Justin Cochangco <88139560+pcochangco@users.noreply.github.com>
2021-10-27 16:15:47 +0800

    Update Camera_test.py

--- testing/Camera_test.py
+++ testing/Camera_test.py
@@ -7,6 +7,7 @@ import os
 def moveCamera():
  
 def ratio_to_actual(image_pixel_ratio):
+    return image_pixel_ratio
 
 def takeImage():
     from picamera import PiCamera
@@ -20,21 +21,28 @@ def takeImage():
     camera.capture(file_name)
     print("Picture taken.")
 
-segment_image = semantic_segmentation()
-segment_image.load_ade20k_model("/deeplabv3_xception65_ade20k.h5")
-directory = "/home/pi/Pictures/"
-overall_ratio = []
-for filename in os.listdir(directory):
-    if filename.endswith(".jpg"):
-        ratio = 0
-        segvalues, output = segment_image.segmentAsAde20k(os.path.join(directory, filename), overlay = True)
-        for item in ["plant", "tree", "grass"]:
-            if item in segvalues["class_names"]:
-                 ratio = ratio + segvalues["ratios"][segvalues["class_names"].index(item)]
-        overall_ratio.append(ratio)
-        cv2.imshow(output)
-image_pixel_ratio = round(sum(overall_ratio)/len(overall_ratio),2)
-print("Average ratio of image to image pixel is", image_pixel_ratio)
-size = ratio_to_actual(image_pixel_ratio)
-print("Image size is", size)
-        
+def imageArea():
+    import pixellib
+    from pixellib.semantic import semantic_segmentation
+    import numpy as np
+    
+    segment_image = semantic_segmentation()
+    segment_image.load_ade20k_model("/deeplabv3_xception65_ade20k.h5")
+    directory = "/home/pi/Pictures/"
+    overall_ratio = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".jpg"):
+            ratio = 0
+            segvalues, output = segment_image.segmentAsAde20k(os.path.join(directory, filename), overlay = True)
+            for item in ["plant", "tree", "grass"]:
+                if item in segvalues["class_names"]:
+                     ratio = ratio + segvalues["ratios"][segvalues["class_names"].index(item)]
+            overall_ratio.append(ratio)
+            #cv2.imshow(output)
+    image_pixel_ratio = round(sum(overall_ratio)/len(overall_ratio),2)
+    print("Average ratio of image to image pixel is " + image_pixel_ratio + "%")
+    size = ratio_to_actual(image_pixel_ratio)
+    print("Image size is", size)
+    
+takeImage()
+imageArea()
