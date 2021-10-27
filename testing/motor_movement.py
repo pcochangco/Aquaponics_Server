56763cff4d375537c3d5dbd4d28151e769a74f29
Philip Justin Cochangco <88139560+pcochangco@users.noreply.github.com>
2021-10-27 15:50:16 +0800

    Update motor_movement.py

--- testing/motor_movement.py
+++ testing/motor_movement.py
@@ -1,61 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 27 15:06:28 2021
+
+@author: pcochang
+"""
+
 import RPi.GPIO as GPIO
 from time import sleep
- 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-Motor1 = {'EN': 25, 'input1': 24, 'input2': 23}
-Motor2 = {'EN': 17, 'input1': 27, 'input2': 22}
+def GPIO_init():
+    global Motor1
+    Motor1 = {'EN': 19, 'input1': 21, 'input2': 22}
+    for x in Motor1:
+        GPIO.setup(Motor1[x], GPIO.OUT)
+    global EN1
+    EN1 = GPIO.PWM(Motor1['EN'], 100)    
+    EN1.start(0)  
 
-for x in Motor1:
-    GPIO.setup(Motor1[x], GPIO.OUT)
-    GPIO.setup(Motor2[x], GPIO.OUT)
-
-EN1 = GPIO.PWM(Motor1['EN'], 100)    
-EN2 = GPIO.PWM(Motor2['EN'], 100)    
-
-EN1.start(0)                    
-EN2.start(0)                    
-
-while True:
-    for x in range(40, 100):
-        print ("FORWARD MOTION")
-        EN1.ChangeDutyCycle(x)
-        EN2.ChangeDutyCycle(x)
-
+def runMotor():
+    print ("Motor running")
+    if True:
+        EN1.ChangeDutyCycle(50) #50% dutycycle
         GPIO.output(Motor1['input1'], GPIO.HIGH)
         GPIO.output(Motor1['input2'], GPIO.LOW)
-        
-        GPIO.output(Motor2['input1'], GPIO.HIGH)
-        GPIO.output(Motor2['input2'], GPIO.LOW)
-
-        sleep(0.1)
-   
+        sleep(3)
+        #if some conditions here:
+            #break
     print ("STOP")
     EN1.ChangeDutyCycle(0)
-    EN2.ChangeDutyCycle(0)
-
-    sleep(5)
-     
-    for x in range(40, 100):
-        print ("BACKWARD MOTION")
-        EN1.ChangeDutyCycle(x)
-        EN2.ChangeDutyCycle(x)
-        
-        GPIO.output(Motor1['input1'], GPIO.LOW)
-        GPIO.output(Motor1['input2'], GPIO.HIGH)
-
-        GPIO.output(Motor2['input1'], GPIO.LOW)
-        GPIO.output(Motor2['input2'], GPIO.HIGH)
-
-        sleep(0.1)
-     
-    print ("STOP")
-    EN1.ChangeDutyCycle(0)
-    EN2.ChangeDutyCycle(0)
-
-    sleep(5)
-
-GPIO.cleanup()
-
     
+GPIO_init()
+runMotor()
